414de0819fe601cdc1112e4dd68d2c807491d19b
MrAleksej <lord_aleksej@mail.ru>
2022-09-15 16:33:29 +0300

    частично решены задачи модуля 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,4 +1,20 @@
 function sumSalary(salaries) {
   // ваш код...
+  salaries = {
+    John: 1000,
+    Ann: 1600,
+    Pete: 1300,
+    month: 'December',
+    currency: 'USD',
+    isPayed: false
+  }
 
+  let sum = 0;
+
+  for (let key in salaries) {
+    if (Number.isFinite(salaries[key])) {
+      sum = sum + salaries[key];
+    }
+  }
+  return sum;
 }
