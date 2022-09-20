a80d6bb75e4ca06393bbd0988415b6c1e7b2500f
MrAleksej <lord_aleksej@mail.ru>
2022-09-21 00:26:29 +0300

    решены три задачи на массивы

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,4 +1,15 @@
 function camelize(str) {
   // ваш код...
-
+  let array1 = str.split('-');
+  let array2 = array1.map(
+    function(word, key) {
+      if (key == 0) {
+        return word
+      }
+      else {
+        return (word[0].toUpperCase() + word.slice(1))
+      }
+    }
+  );
+  return array2.join('');
 }
