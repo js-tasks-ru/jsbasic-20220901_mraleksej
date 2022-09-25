a1abbe05569e4fbaa7453cb36a01d3033be5b2b3
MrAleksej <lord_aleksej@mail.ru>
2022-09-25 14:53:15 +0300

    решение задач на массивы

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
   // ваш код...
+  const result = {};
+  str.replace(/\S+/g, (n) => {
+    n = +n;
+    (!("min" in result) || result.min > n) && (result.min = n);
+    (!("max" in result) || result.max < n) && (result.max = n);
+  });
+  return result;
 }
