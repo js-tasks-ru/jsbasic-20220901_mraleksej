221f1452e54fea402fc6e21c68304a3cec7eda5e
MrAleksej <lord_aleksej@mail.ru>
2022-10-02 14:29:52 +0300

    решения задач модуля 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const list = document.createElement('UL')
+  for (let name of friends) {
+    list.innerHTML = list.innerHTML + `<li>${name.firstName} ${name.lastName}</li>`
+  }
+  return list
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
   // ваш код...
+  for (let row of table.rows) {
+
+    for (i = 0; i < row.cells.length; i++) {
+      if (row.cells[i].dataset.available == "true") {
+        row.classList.add("available");
+      } else if (row.cells[i].dataset.available == "false") {
+        row.classList.add("unavailable");
+      } else if ((row.cells[i].dataset.available == null)) {
+        row.setAttribute("hidden", "hidden");
+      }
+    }
+
+    if (row.cells[2].innerHTML == "m") {
+      row.classList.add("male");
+    } else if (row.cells[2].innerHTML == "f") {
+      row.classList.add("female");
+    }
+
+    if (row.cells[1].innerHTML < 18) {
+      row.style.textDecoration = "line-through";
+    }
+
+  }
 }
